daff9626631e2b81c052331f9846cc7a965ce721
Aravinth PM <110657801+aravinthpm77@users.noreply.github.com>
2025-06-24 13:48:40 +0530

    Create 23.06.25_Single_Number.java

---
+++ Array/23.06.25_Single_Number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            boolean isUnique = true;
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[i] == nums[j]) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                return nums[i];
+            }
+        }
+        return -1; 
+        
+    }
+}
